7ab1ca3a37ecddc6b59dfbb37191b9e3784a7884
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-09-20 18:30:13 +0500

    Clear project

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "vuex";
-import { PostModule } from "@/store/PostModule";
-import { CommentsModule } from "@/store/CommentsModule";
-export default createStore({
-    modules: {
-        post: PostModule,
-        comments: CommentsModule,
-    },
-});
